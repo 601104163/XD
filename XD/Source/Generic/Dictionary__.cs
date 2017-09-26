a771d39a2f986c3d6905eaa2da130871c2d02783
wxiaodi <w601104163@live.com>
2017-09-26 18:21:55 +0800

    添加项目

---
+++ XD/Source/Generic/Dictionary__.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+
+namespace XD.Generic
+{
+    class Dictionary__<T_KEY, T_VALUE> : Dictionary<T_KEY, T_VALUE>
+    {
+        public short GetInt16(T_KEY key)
+        {
+            string s = this[key].ToString();
+            short ret;
+            short.TryParse(s, out ret);
+            return ret;
+        }
+
+        public int GetInt32(T_KEY key)
+        {
+            string s = this[key].ToString();
+            int ret;
+            int.TryParse(s, out ret);
+            return ret;
+        }
+
+        public long GetInt64(T_KEY key)
+        {
+            string s = this[key].ToString();
+            long ret;
+            long.TryParse(s, out ret);
+            return ret;
+        }
+
+        public ushort GetUInt16(T_KEY key)
+        {
+            string s = this[key].ToString();
+            ushort ret;
+            ushort.TryParse(s, out ret);
+            return ret;
+        }
+
+        public uint GetUInt32(T_KEY key)
+        {
+            string s = this[key].ToString();
+            uint ret;
+            uint.TryParse(s, out ret);
+            return ret;
+        }
+
+        public ulong GetUInt64(T_KEY key)
+        {
+            string s = this[key].ToString();
+            ulong ret;
+            ulong.TryParse(s, out ret);
+            return ret;
+        }
+
+        public float GetFloat(T_KEY key)
+        {
+            string s = this[key].ToString();
+            float ret;
+            float.TryParse(s, out ret);
+            return ret;
+        }
+
+        public double GetDouble(T_KEY key)
+        {
+            string s = this[key].ToString();
+            double ret;
+            double.TryParse(s, out ret);
+            return ret;
+        }
+
+        public char GetChar(T_KEY key)
+        {
+            string s = this[key].ToString();
+            char[] _charArray = s.ToCharArray();
+            char ret = '\0';
+            if (_charArray.Length > 0)
+                ret = _charArray[0];
+            return ret;
+        }
+    }
+}
